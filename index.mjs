e96c65eeb16dc38e1f0eceff893780b1fab4127b
aaron <aaron@pop-os.localdomain>
2021-09-30 19:01:16 -0400

    fixed bot login info

--- index.mjs
+++ index.mjs
@@ -1,8 +1,25 @@
 import { Client, Intents } from "discord.js";
+import fetch from "node-fetch";
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAG.GUILD_MESSAGES], });
+const client = new Client({ 
+	intents: [Intents.FLAGS.GUILDS, Intents.FLAG.GUILD_MESSAGES],
+});
 
 client.once("ready", () => {
-	console.log("Bot is ready!")
-});
+	console.log("Bot is ready!");
+})
+
+client.on("messageCreate", async (message) => {
+	const regex = /https?:\/\/github.com\/([\d\w-\.]+\/[\d\w-\.]+)/;
+	const matches = message.content.match(regex);
+	if (matches !== null) {
+		const repo = await fetch("https://api.guthub.com/repos/" + matches[1]).then()
+		console.log(repo);
+		if (repo.license === null) {
+			message.reply("You don't have a license!");
+		}
+	}
+})
+
+client.login("1c799aaa762136f446df2717e4ce6796607d0730882bae955a46cbeca0302082");
 
